546dc627907680b23630afbd7f830f035d0f6c4e
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2024-01-18 12:20:38 +0100

    Update server.py

--- server.py
+++ server.py
@@ -59,44 +59,4 @@ def generate_token():
     # Return the token if the client is logged in, otherwise return error
     return jsonify({"token": token, "error": False}) if client.logged_in else jsonify({token: False, "error": "Login failed"}), 200 if client.logged_in else 498
 
-# Route to get user data
-@app.route('/user', methods=['GET'])
-@token_required
-def user():
-    # Get token and client from request arguments and saved clients
-    token = request.args.get('token')
-    client = saved_clients[token]['client']
-
-    # Define keys to get from client info
-    client_info_keys = [
-        "name",
-        "class_name",
-        "establishment",
-        "phone",
-        "email",
-        "address",
-        "ine_number",
-        "profile_picture",
-        "delegue"
-    ]
-    
-    # Get client info
-    client_info = {
-        key: getattr(client.info, key, "") for key in client_info_keys
-    }
-
-    # Convert profile picture to url if it exists
-    client_info["profile_picture"] = client_info["profile_picture"].url if client_info["profile_picture"] else None
-
-    # Get user type and children if user is a parent
-    usertype = type(client).__name__
-    children = client.children.to_dict() if usertype == "ParentClient" else []
-
-    # Combine all user data
-    userData = {
-        **client_info,
-        "client": {"type": usertype, "children": children}
-    }
-
-    # Return user data if client is logged in, otherwise return error
-    return userData if client.logged_in else jsonify({"error": "expired"}), 498
+import routes.user
